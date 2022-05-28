1797f8da70c642201a1c8fc0e6343bf7b25cbd10
Martin Pool <mbp@sourcefrog.net>
2022-05-28 16:41:47 -0700

    AES-CBC encryption

--- src/aes.rs
+++ src/aes.rs
@@ -2,9 +2,11 @@
 
 use aes::cipher::consts::U16;
 use aes::cipher::generic_array::GenericArray;
-use aes::cipher::{BlockDecrypt, KeyInit};
+use aes::cipher::{BlockDecrypt, BlockEncrypt, KeyInit};
 use aes::Aes128;
 use rand::prelude::*;
+
+use crate::pkcs7;
 
 const BLK: usize = 16;
 
@@ -23,7 +25,7 @@ impl Key {
     }
 }
 
-pub fn decrypt_aes_cbc(ct: &[u8], iv: &[u8], key: &Key) -> Vec<u8> {
+pub fn decrypt_aes_cbc(ct: &[u8], iv: &[u8], key: &Key) -> Option<Vec<u8>> {
     let cipher = Aes128::new(&key.0);
     let mut last_block: &[u8] = iv;
     let mut plain: Vec<u8> = Vec::with_capacity(ct.len());
@@ -37,5 +39,42 @@ pub fn decrypt_aes_cbc(ct: &[u8], iv: &[u8], key: &Key) -> Vec<u8> {
         plain.extend(&buf);
         last_block = block;
     }
-    plain
+    pkcs7::unpad(&plain).map(|s| s.to_owned())
+}
+
+pub fn encrypt_aes_cbc(plain: &[u8], iv: &[u8], key: &Key) -> Vec<u8> {
+    let mut plain = plain.to_owned();
+    pkcs7::pad(&mut plain, BLK);
+    let cipher = Aes128::new(&key.0);
+    let mut prev_ct: GenericArray<u8, U16> = GenericArray::clone_from_slice(iv);
+    let mut ct: Vec<u8> = Vec::with_capacity(plain.len());
+    let mut buf: GenericArray<u8, U16> = GenericArray::default();
+    for block in plain.chunks(BLK) {
+        buf.copy_from_slice(block);
+        for i in 0..BLK {
+            buf[i] ^= prev_ct[i];
+        }
+        cipher.encrypt_block(&mut buf);
+        ct.extend(&buf);
+        prev_ct.clone_from(&buf);
+    }
+    ct
+}
+
+#[cfg(test)]
+mod test {
+    use proptest::prelude::*;
+
+    use super::*;
+
+    proptest! {
+        #[test]
+        fn roundtrip(plain: Vec<u8>) {
+            let key = Key::random();
+            let iv = [0u8; 16];
+            let ct = encrypt_aes_cbc(&plain, &iv, &key);
+            let ret = decrypt_aes_cbc(&ct, &iv, &key).expect("decryption failed");
+            assert_eq!(plain, ret);
+        }
+    }
 }
